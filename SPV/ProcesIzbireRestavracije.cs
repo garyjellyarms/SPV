7567f8dfb07c7035bbdac704c6301e2b3e7c33f4
regy647 <matic.regally@gmail.com>
2023-03-11 21:14:21 +0100

    User CRUD

---
+++ SPV/ProcesIzbireRestavracije.cs
@@ -0,0 +1,77 @@
+﻿namespace SPV
+{
+    public class ProcesIzbireRestavracije
+    {
+        //grobi potek algoritma
+
+        //vstopita dve izbiri (hrana)
+
+        //uporabnik izbere eno izmed njih
+
+        //izbrana opcija gre v 2. krog
+
+        //2. KROG
+        //vstopi izbrana hrana in nova opcija (random)
+
+        //uporabnik izbere eno izmed njih
+
+        //izbrana opcija gre v 3. krog
+
+        //3. KROG
+        //vstopi izbrana hrana in nova opcija (random)
+
+        //uporabnik izbere eno izmed njih
+
+        //izbrana opcija gre v 4. krog
+
+        //4. KROG
+        //vstopi izbrana hrana in nova opcija (random)
+
+        //uporabnik izbere eno izmed njih
+
+        //izbrana opcija gre v 5. krog
+
+        //5. KROG
+        //vstopi izbrana hrana in nova opcija (random)
+
+        //uporabnik izbere eno izmed njih
+
+        //izbrana opcija zmaga 
+
+        //poiscejo se tri restavracije ki vsebujejo to hrano na svojem seznamu
+        //*************KAKO SE BO GLEDAL KERE RESTAVRACIJE SO NAJBLIZJE? OZ. V BLIZINI?*********//
+
+        //*************KJE SE BOJO SHRANJEVALE TE OPCIJE MED IZVAJANJEM PROGRAMA ??????*********//
+
+
+
+
+
+
+        //*************KAKO SE BO IZBRTAL RESTAVRACIJO*********//
+
+        //Pridobimo vse restavracije 
+
+        //iteriramo čez vse restavracije
+        //znotrej loopa preberemo json vsake
+        //ga serealisamo 
+        //if loop da preveri ce se izbrana hrana pojavi na  seznamu
+        //ce se pojavi jo dodamo na list vjemajocih restavracij
+
+
+
+        //*************PREVERJANJE ODPRTOSTI RESTAVRACIJE*********//
+
+
+        //*************PREVERJANJE ODPRTOSTI RESTAVRACIJE*********//
+
+
+        //*************PRIDOBITEV TRENUTNE LOKACIJE UPORABNIKA*********//
+
+
+        //*************PREVERJANJE ODALJENOSTI RESTAVRACIJE*********//
+
+
+        //*************RAZVRSCANJE LOKACIJ GLEDE NA RAZDALJO OD UPORABNIKA*********//
+    }
+}
